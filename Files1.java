de82eb5a0c11d81b6bb0b12f5f96f899a218f0f7
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-20 22:53:30 +0300

    Added file class

---
+++ Files1.java
@@ -0,0 +1,21 @@
+
+import java.io.File;
+import java.io.IOException;
+
+public class Files1{
+
+    public static void main(String[] args) {
+        
+        File file = new File("text.txt");
+
+        try {
+            if (file.createNewFile()){
+                System.out.println("Created file "+file.getName());
+            }else{
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating file");
+        }
+    }
+}
